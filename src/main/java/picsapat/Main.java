6ff21f12dd8bafd70bd7594efdc0ddd9f266c1a1
Attila Juhos <juhosattila6@gmail.com>
2019-02-28 20:01:31 +0100

    Vertical matching started.

--- src/main/java/picsapat/Main.java
+++ src/main/java/picsapat/Main.java
@@ -11,8 +11,55 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Photo> photos= getInput(new File("b_lovely_landscapes.txt"));
+		algo("a_example.txt", "out_a");
+//		algo("b_lovely_landscapes.txt", "out_b");
+//		algo("c_memorable_moments.txt", "out_c");
+//		algo("d_pet_pictures.txt", "out_d");
+//		algo("e_shiny_selfies.txt", "out_e");
+	}
+	
+	public static void algo(String inputFileName, String outputFileName) {
+		List<Photo> photos= getInput(new File(inputFileName));
+
+		List<Photo> horizontalPhotos = filterOrientation(photos, Orientation.HORIZONTAL);
+		List<Photo> verticalPhotos = filterOrientation(photos, Orientation.VERTICAL);
+		List<Slide> horizontalSlides = convertHorizontalToSlides(horizontalPhotos);
+		List<Slide> matchedVerticals = matchVerticalsToSlides(verticalPhotos);
+		List<Slide> unorderedSlides = createUnorderedSlides(horizontalSlides, matchedVerticals);
+	}
+
+
+	private static List<Slide> createUnorderedSlides(List<Slide> horizontalSlides, List<Slide> matchedVerticals) {
+		List<Slide> slides = new ArrayList<Slide>();
 		
+		slides.addAll(horizontalSlides);
+		slides.addAll(matchedVerticals);
+		
+		return slides.
+	}
+
+	private static List<Slide> convertHorizontalToSlides(List<Photo> horizontalPhotos) {
+		List<Slide> slides = new ArrayList<Slide>();
+		horizontalPhotos.forEach( (Photo p) -> slides.add(new HorizontalSlide(p)) );
+		return null;
+	}
+
+
+	private static List<Slide> matchVerticalsToSlides(List<Photo> verticalPhotos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private static List<Photo> filterOrientation(List<Photo> photos, Orientation o) {
+		List<Photo> filtered = new ArrayList<Photo>();
+		
+		for(Photo p : photos) {
+			if(p.orientation == o)
+				filtered.add(p);
+		}
+		
+		return filtered;
 	}
 	
 	
