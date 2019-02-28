fa19776ee15b190cc094f6a24b8fd64e49d55573
bduvid@gmail.com <bduvid@gmail.com>
2019-02-28 19:55:00 +0100

    Utils changed

--- src/main/java/picsapat/TagUtils.java
+++ src/main/java/picsapat/TagUtils.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.Set;
 
 public class TagUtils {
-	public static Set<String> union(Photo photo1, Photo photo2){
-		Set<String> union = new HashSet<String>(Math.max(photo1.tags.size(), photo2.tags.size()));
-		for (String string : photo1.tags) {
+	public static Set<String> union(Set<String> photo1, Set<String> photo2){
+		Set<String> union = new HashSet<String>(Math.max(photo1.size(), photo2.size()));
+		for (String string : photo1) {
 			if(!union.contains(string)) {
 				union.add(string);
 			}
 		}
-		for (String string : photo2.tags) {
+		for (String string : photo2) {
 			if(!union.contains(string)) {
 				union.add(string);
 			}
